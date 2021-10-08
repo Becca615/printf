9c0d9edb0cc99a3072ecdf7e2372ac0389b3f957
Yezeed <yexxy94@yahoo.com>
2021-10-08 06:33:14 -0700

    Print short decimal in hex

--- print_short_upx.c
+++ print_short_upx.c
@@ -1,4 +1,5 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
  * prinhupx - prints a short decimal in hexadecimal
  * @arguments: The character to print
@@ -6,6 +7,7 @@
  * @ibuf: index for buffer pointer
  * Return: number of chars printed
  */
+
 int prinhupx(va_list arguments, char *buf, unsigned int ibuf)
 {
 	short int int_input, i, isnegative, count, first_digit;

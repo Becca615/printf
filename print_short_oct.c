32ddf5193f5502d9884ac511ccdc56242b4b4b01
Yezeed <yexxy94@yahoo.com>
2021-10-08 06:26:33 -0700

    Print ling decimal num in octal

--- print_short_oct.c
+++ print_short_oct.c
@@ -1,4 +1,5 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
  * prinhoct - prints long decimal number in octal
  * @arguments: input number
@@ -6,6 +7,7 @@
  * @ibuf: index for buffer pointer
  * Return: number of chars printed.
  */
+
 int prinhoct(va_list arguments, char *buf, unsigned int ibuf)
 {
 	short int int_input, i, isnegative, count, first_digit;

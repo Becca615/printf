a4ed1b06678e4b909b413d42215c0696cdc9abe9
Yezeed <yexxy94@yahoo.com>
2021-10-08 06:46:27 -0700

    Print decimal in hex

--- print_upx.c
+++ print_upx.c
@@ -1,4 +1,5 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
  * print_upx - prints a decimal in hexadecimal
  * @arguments: The character to print
@@ -6,6 +7,7 @@
  * @ibuf: index for buffer pointer
  * Return: number of chars printed
  */
+
 int print_upx(va_list arguments, char *buf, unsigned int ibuf)
 {
 	int int_input, i, isnegative, count, first_digit;
